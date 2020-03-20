9502e3d88f3bd7541b7a56d865974440033591a4
Adrianna Gryglak <adrianna.juda@live.com>
2020-03-20 15:43:19 +0100

    Divide script.js into components

---
+++ src/js/components/Cart.js
@@ -0,0 +1,145 @@
+import {settings, select, classNames, templates} from '../settings.js';
+import {utils} from '../utils.js';
+import {CartProduct} from './CartProduct.js';
+
+export class Cart {
+  constructor(element) {
+    const thisCart = this;
+
+    thisCart.products = [];
+    thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+
+    thisCart.getElements(element);
+    thisCart.initActions();
+
+    //console.log('new cart:', thisCart);
+  }
+
+  getElements(element) {
+    const thisCart = this;
+
+    thisCart.dom = {};
+    thisCart.dom.wrapper = element;
+
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+    thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+    thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+    thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
+
+    thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+
+    for (let key of thisCart.renderTotalsKeys) {
+      thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+    }
+
+  }
+
+  initActions() {
+    const thisCart = this;
+
+    thisCart.dom.toggleTrigger.addEventListener('click', function () {
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
+
+    thisCart.dom.productList.addEventListener('updated', function () {
+      thisCart.update();
+    });
+
+    thisCart.dom.productList.addEventListener('remove', function () {
+      thisCart.remove(event.detail.cartProduct);
+    });
+    thisCart.dom.form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      thisCart.sendOrder();
+    });
+  }
+
+  add(menuProduct) {
+    const thisCart = this;
+
+    const generatedHTML = templates.cartProduct(menuProduct); //skad jest menuproduct?
+
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+    thisCart.dom.productList.appendChild(generatedDOM);
+
+    // thisCart.products.push(menuProduct); this should existed earlier
+
+    thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+
+    console.log('adding product', menuProduct);
+
+    thisCart.update();
+  }
+
+  update() {
+    const thisCart = this;
+
+    thisCart.totalNumber = 0;
+    thisCart.subtotalPrice = 0;
+
+    for (let product of thisCart.products) {
+      thisCart.subtotalPrice += product.price;
+      thisCart.totalNumber += product.amount;
+    }
+
+    thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+
+    for (let key of thisCart.renderTotalsKeys) {
+      for (let elem of thisCart.dom[key]) {
+        elem.innerHTML = thisCart[key];
+      }
+    }
+  }
+
+  remove(cartProduct) {
+    const thisCart = this;
+
+    const index = thisCart.products.indexOf(cartProduct);
+
+    thisCart.products.splice(index, 1);
+
+    cartProduct.dom.wrapper.remove();
+
+    thisCart.update();
+  }
+
+  sendOrder() {
+    const thisCart = this;
+    const url = settings.db.url + '/' + settings.db.order;
+
+    const payload = {
+      adress: thisCart.dom.address.value,
+      phone: thisCart.dom.phone.value,
+      totalNumber: thisCart.totalNumber,
+      subtotalPrice: thisCart.subtotalPrice,
+      totalPrice: thisCart.totalPrice,
+      deliveryFee: thisCart.deliveryFee,
+      products: [],
+    };
+
+    for (let product of thisCart.products) {
+      product.getData();
+      delete product.dom;
+      delete product.amountWidget;
+      payload.products.push(product);
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, options)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+
+}
